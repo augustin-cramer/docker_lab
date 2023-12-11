df0721e1e874b22c9771f2419065e0d89dee06ea
Augustin Cramer <augustin.cramer@qantev.com>
2023-12-11 17:39:15 +0100

    first commit

---
+++ jovian-careers-website/app.py
@@ -0,0 +1,43 @@
+from flask import Flask, render_template, jsonify
+from db_utils import initialize_database, get_mysql_connection
+from config import MYSQL_HOST, MYSQL_USER, MYSQL_PASSWORD, MYSQL_DB, DATA_FILE_NAME, WAIT_TIME
+import os
+import time
+
+app = Flask(__name__)
+
+# MySQL configurations
+app.config['MYSQL_HOST'] = os.environ.get('MYSQL_HOST', MYSQL_HOST)
+app.config['MYSQL_USER'] = os.environ.get('MYSQL_USER', MYSQL_USER)
+app.config['MYSQL_PASSWORD'] = os.environ.get('MYSQL_PASSWORD', MYSQL_PASSWORD)
+app.config['MYSQL_DB'] = os.environ.get('MYSQL_DB', MYSQL_DB)
+app.config['DATA_FILE_NAME'] = os.environ.get('DATA_FILE_NAME', DATA_FILE_NAME)
+app.config['WAIT_TIME'] = int(os.environ.get('WAIT_TIME', WAIT_TIME))
+
+# Wait for MySQL to start
+time.sleep(app.config['WAIT_TIME'])
+
+# Initialize the database
+initialize_database(app)
+
+
+@app.route("/")
+def hello_jovian():
+  with get_mysql_connection(app) as conn:
+    cur = conn.cursor(dictionary=True)
+    cur.execute("SELECT * FROM jobs")
+    data = cur.fetchall()
+  return render_template('home.html', 
+                          jobs=data, 
+                          company_name='Jovian')
+
+@app.route("/api/jobs")
+def list_jobs():
+  with get_mysql_connection(app) as conn:
+    cur = conn.cursor(dictionary=True)
+    cur.execute("SELECT * FROM jobs")
+    data = cur.fetchall()
+  return jsonify(data)
+
+if __name__ == '__main__':
+  app.run(host='0.0.0.0', debug=True, port=5001)
---
+++ jovian-careers-website/db_utils.py
@@ -0,0 +1,78 @@
+import mysql.connector
+import json
+import time
+import logging
+
+logging.basicConfig(level=logging.INFO)
+
+# doesn't work currently
+def wait_for_mysql(mysql):
+    while True:
+        try:
+            mysql.ping(reconnect=True, attempts=5, delay=1)
+            print("Connected to MySQL")
+            break
+        except mysql.connector.Error as e:
+            print(f"Failed to connect to MySQL: {e}")
+            time.sleep(1)
+
+# Function to initialize the database
+def initialize_database(app):
+    try:
+        # Create a connection without specifying the database
+        conn = mysql.connector.connect(
+            host=app.config['MYSQL_HOST'],
+            user=app.config['MYSQL_USER'],
+            password=app.config['MYSQL_PASSWORD']
+        )
+        # wait_for_mysql(conn)
+        # Create a cursor to execute SQL commands
+        cursor = conn.cursor()
+
+        # Create the database if it doesn't exist
+        cursor.execute(f"CREATE DATABASE IF NOT EXISTS {app.config['MYSQL_DB']};")
+        logging.info(f"Created database {app.config['MYSQL_DB']}")
+
+        # Switch to the database
+        cursor.execute(f"USE {app.config['MYSQL_DB']};")
+
+        # Drop the table if it exists
+        cursor.execute("DROP TABLE IF EXISTS jobs;")
+
+        # Create a new table
+        cursor.execute("""
+            CREATE TABLE jobs (
+                id INT AUTO_INCREMENT PRIMARY KEY,
+                title VARCHAR(255),
+                location VARCHAR(255),
+                salary VARCHAR(255)
+            );
+        """)
+
+        # Read data from jobs.json
+        with open(app.config['DATA_FILE_NAME']) as json_file:
+            jobs_data = json.load(json_file)
+
+        # Insert data into the table
+        for job in jobs_data:
+            cursor.execute("""
+                INSERT INTO jobs (title, location, salary)
+                VALUES (%s, %s, %s)
+            """, (job['title'], job['location'], job.get('salary', '')))
+
+        # Commit the changes and close the connection
+        conn.commit()
+        conn.close()
+
+        print("Database initialized successfully.")
+
+    except mysql.connector.Error as e:
+        print(f"Error initializing database: {e}")
+        
+def get_mysql_connection(app):
+  return  mysql.connector.connect(
+      host=app.config['MYSQL_HOST'],
+      user=app.config['MYSQL_USER'],
+      password=app.config['MYSQL_PASSWORD'],
+      database=app.config['MYSQL_DB']
+  )
