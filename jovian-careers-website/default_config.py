f42cbe2ca5d5163c11359fe75806ba54bf029663
Augustin Cramer <augustin.cramer@qantev.com>
2023-12-12 14:39:27 +0100

    reformat using black

--- jovian-careers-website/default_config.py
+++ jovian-careers-website/default_config.py
@@ -1,6 +1,6 @@
-MYSQL_HOST = 'mysql'
-MYSQL_USER = 'root'
-MYSQL_PASSWORD = 'root'
-MYSQL_DB = 'jovian_careers_default'
-DATA_FILE_NAME = 'jobs_1.json'
+MYSQL_HOST = "mysql"
+MYSQL_USER = "root"
+MYSQL_PASSWORD = "root"
+MYSQL_DB = "jovian_careers_default"
+DATA_FILE_NAME = "jobs_1.json"
 WAIT_TIME = 0
